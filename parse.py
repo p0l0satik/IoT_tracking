25b456d754414d710fc28d6e70d79cf0195926f6
p0l0satik <denis.cucu@yandex.ru>
2021-11-23 14:07:48 +0300

    made template for parer of patterns

--- parse.py
+++ parse.py
@@ -7,18 +7,18 @@ def approx_hrm(arr):
     return sum(int_arr) / len(arr)
 
 
-def get_period_aprox(kill, interval, file, pattern):
+def get_period_aprox(timestamp, interval, file, pattern):
     res = []
-    # time_H_M = datetime.strftime(kill, "%H:%M")
+    # time_H_M = datetime.strftime(timestamp, "%H:%M")
     with open(hrm) as file:
         line = file.readline()
-        while(line.find(datetime.strftime(kill, "%H:%M:%S")) == -1):
+        while(line.find(datetime.strftime(timestamp, "%H:%M:%S")) == -1):
             line = file.readline()
             if (not len(line)):
                 return []
 
         for sec in range(interval):
-            start = kill + timedelta(seconds=sec)
+            start = timestamp + timedelta(seconds=sec)
             arr = []
             while(line.find(datetime.strftime(start, "%H:%M:%S")) > 0):
                 val = line.split(",")[1]
@@ -32,6 +32,17 @@ def get_period_aprox(kill, interval, file, pattern):
             
         return res 
 
+def parce_pattern(path_to_fig, time_int, timestamps, pat_type):
+    final_res = []
+    half = time_int // 2
+    if not os.path.exists(path_to_fig):
+        os.mkdir(path_to_fig)
+    for timestamp in timestamps:
+        timestamp_time = datetime.strptime(timestamp, "%H:%M:%S")
+        meas_start = timestamp_time - timedelta(seconds=half)
+        res = get_period_aprox(meas_start, half, file, pat_type) + get_period_aprox(timestamp_time, half, file, pat_type)
+        final_res.append(res)
+    return final_res
      
 
 if __name__ == "__main__":
@@ -46,20 +57,11 @@ if __name__ == "__main__":
                 time = line[15:23]
                 kills.append(time)
 
-    final_res = []
-    time_int = 60
-    half = time_int // 2
-    path_to_fig = "kills_hrm"
-    if not os.path.exists(path_to_fig):
-        os.mkdir(path_to_fig)
-    for kill in kills:
-        kill_time = datetime.strptime(kill, "%H:%M:%S")
-        meas_start = kill_time - timedelta(seconds=half)
-        res = get_period_aprox(meas_start, half, file, "hrm") + get_period_aprox(kill_time, half, file, "hrm")
-        final_res.append(res)
+    kills_parsed = parce_pattern("kills_hrm", 60, kills, "hrm")
+    for n, stamp in enumerate(kills):
         plt.clf()
-        plt.plot(res)
-        plt.savefig(os.path.join(path_to_fig, kill + ".png"))
+        plt.plot(kills_parsed[n])
+        plt.savefig(os.path.join("kills_hrm", stamp + ".png"))
 
 
 
